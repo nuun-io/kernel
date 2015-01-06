5db9e32f81d1f5333423e02f34efc2305fb891de
ejemba <epo@jemba.net>
2015-01-06 12:37:41 +0100

    Kernel Params on IT : Pull Request #18
    Squashed commit of the following:
    
    commit 9696a6ddc1df6757d8ba7d7d4f47ea5a64669ead
    Author: pith <pierre.thirouin@gmail.com>
    Date:   Sun Dec 21 13:52:28 2014 +0100
    
        Added @WithParam to specify kernel params in IT.

---
+++ tests/src/main/java/io/nuun/kernel/tests/it/annotations/WithParams.java
@@ -0,0 +1,24 @@
+package io.nuun.kernel.tests.it.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * This annotation can be used on integration tests to specify kernel parameters.
+ *
+ * @author Pierre Thirouin <pierre.thirouin@gmail.com>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface WithParams {
+
+    /**
+     * List of kernel parameters as key/value pairs.
+     * <pre>
+     * {@literal @}WithParams("key1", "val1", "key2", "val2")
+     * </pre>
+     *
+     * @return key/value pairs
+     */
+    String[] value();
+}
