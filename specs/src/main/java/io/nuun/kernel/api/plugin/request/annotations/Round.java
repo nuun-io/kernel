5965e7afc42b6d6fe654083bd9e5b124abb8278e
pith <pierre.thirouin@ext.mpsa.com>
2015-10-06 17:45:25 +0200

    Add annotation API
    This reverts commit e9388d38cfdbcf3c1f20c18aa7de9ddd0d554ec8.

---
+++ specs/src/main/java/io/nuun/kernel/api/plugin/request/annotations/Round.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2014 Kametic <epo.jemba@kametic.com>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * or any later version
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.nuun.kernel.api.plugin.request.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * 
+ * Round annotation. Round number starts with 0.
+ * 
+ * @author epo.jemba{@literal @}kametic.com
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD,ElementType.METHOD})
+@Qualifier
+public @interface Round
+{
+    /**
+     * @return the number of the round.
+     */
+    int value() default 1;
+}
---
+++ specs/src/test/java/io/nuun/kernel/api/plugin/request/annotations/TestPlugin.java
@@ -0,0 +1,209 @@
+/**
+ * Copyright (C) 2014 Kametic <epo.jemba@kametic.com>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * or any later version
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.nuun.kernel.api.plugin.request.annotations;
+
+import io.nuun.kernel.api.Plugin;
+import io.nuun.kernel.api.di.UnitModule;
+import io.nuun.kernel.api.plugin.InitState;
+import io.nuun.kernel.api.plugin.RoundEnvironment;
+import io.nuun.kernel.api.plugin.context.Context;
+import io.nuun.kernel.api.plugin.context.InitContext;
+import io.nuun.kernel.api.plugin.request.*;
+import io.nuun.kernel.spi.DependencyInjectionProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.*;
+
+/**
+ * @author epo.jemba{@literal @}kametic.com
+ */
+public abstract class TestPlugin implements Plugin
+{
+    Logger                      logger           = LoggerFactory.getLogger(TestPlugin.class);
+
+    Context                     context          = null;
+    Object                      containerContext = null;
+    RoundEnvironment            roundEnvironment = null;
+    Map<String, String>         kernelParams     = null;
+    InitContext                 initContext      = null;
+    KernelParamsRequestBuilder  paramsBuilder    = null;
+    ClasspathScanRequestBuilder scanBuilder      = null;
+    BindingRequestBuilder       bindingBuilder   = null;
+
+    /**
+     * 
+     */
+    public TestPlugin()
+    {
+        paramsBuilder = new KernelParamsRequestBuilder();
+        scanBuilder = new ClasspathScanRequestBuilder();
+        bindingBuilder = new BindingRequestBuilder();
+    }
+
+    /**
+     * ============================= PLUGIN LIFE CYCLE USED BY KERNEL =============================
+     * 
+     * @return
+     **/
+
+    @Override
+    public InitState init(InitContext initContext)
+    {
+        this.initContext = initContext;
+
+        return InitState.INITIALIZED;
+    }
+
+    @Override
+    public void stop()
+    {
+    }
+
+    @Override
+    public void start(Context context)
+    {
+        this.context = context;
+    }
+
+    // /**
+    // * ============================= PLUGIN Utilities Helpers =============================
+    // *
+    // *
+    // **/
+
+    // * ============================= PLUGIN info and requests * ============================= //
+
+    @Override
+    public abstract String name();
+
+    @Override
+    public String description()
+    {
+        return name() + " Nuun Based Plugin.";
+    }
+
+    @Override
+    public Collection<KernelParamsRequest> kernelParamsRequests()
+    {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Collection<ClasspathScanRequest> classpathScanRequests()
+    {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Collection<BindingRequest> bindingRequests()
+    {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Collection<Class<? extends Plugin>> requiredPlugins()
+    {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Collection<Class<? extends Plugin>> dependentPlugins()
+    {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String pluginPropertiesPrefix()
+    {
+        return "";
+    }
+
+    @Override
+    public String pluginPackageRoot()
+    {
+        return "";
+    }
+
+    @Override
+    public UnitModule unitModule()
+    {
+        return null;
+    }
+
+    @Override
+    public UnitModule overridingUnitModule()
+    {
+        return null;
+    }
+
+    @Override
+    public void provideContainerContext(Object containerContext)
+    {
+        this.containerContext = containerContext;
+    }
+
+    @Override
+    public Set<URL> computeAdditionalClasspathScan()
+    {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public DependencyInjectionProvider dependencyInjectionProvider()
+    {
+        return null;
+    }
+
+    @Override
+    public void provideRoundEnvironment(RoundEnvironment roundEnvironment)
+    {
+        this.roundEnvironment = roundEnvironment;
+
+    }
+
+    @Override
+    public Map<String, String> kernelParametersAliases()
+    {
+        return new HashMap<String, String>();
+    }
+
+    protected Collection<Class<? extends Plugin>> collectionOf(Class<? extends Plugin>... items)
+    {
+        return Arrays.asList(items);
+    }
+
+    protected UnitModule unitModule(final Object module)
+    {
+        return new UnitModule()
+        {
+
+            @Override
+            public Object nativeModule()
+            {
+                return module;
+            }
+
+            @Override
+            public <T> T as(Class<T> targetType)
+            {
+                return targetType.cast(module);
+            }
+        };
+    }
+}
