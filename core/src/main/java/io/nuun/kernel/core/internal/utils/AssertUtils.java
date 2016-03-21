c404da370dad3f7faa7d362a96adb1b420a74955
pith <pierre.thirouin@ext.mpsa.com>
2016-03-21 15:26:42 +0100

    Add mvn license plugin

--- core/src/main/java/io/nuun/kernel/core/internal/utils/AssertUtils.java
+++ core/src/main/java/io/nuun/kernel/core/internal/utils/AssertUtils.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (C) 2013-2014 Kametic <epo.jemba@kametic.com>
+ * This file is part of Nuun IO Kernel Core.
  *
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * or any later version
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Nuun IO Kernel Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * Nuun IO Kernel Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Nuun IO Kernel Core.  If not, see <http://www.gnu.org/licenses/>.
  */
 package io.nuun.kernel.core.internal.utils;
 
